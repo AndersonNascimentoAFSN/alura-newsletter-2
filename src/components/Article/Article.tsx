0bf159919afe67cabd3a3d345df0a4b731e4d796
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-04-01 00:35:35 -0300

    feat: css image

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -14,6 +14,9 @@ export function Article({ title, text, tags, image, alt }: ArticleProps) {
       <div className="grid gap-1">
         {text.map((content) => <p key={content} className="text-alura-200 dark:text-gray-400">{content}</p>)}
       </div>
+
+      {image && <img src={image} className="sm:p-4"/>}
+      {image && alt && <span className="sr-only">{alt}</span>}
     </div>
   )
 }
