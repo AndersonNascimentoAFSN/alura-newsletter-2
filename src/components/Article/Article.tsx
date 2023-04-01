623c9cc30510a45a5d34a884507ccdf046c58572
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-04-01 00:42:07 -0300

    feat: list tags

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -8,14 +8,20 @@ type ArticleProps = {
 
 export function Article({ title, text, tags, image, alt }: ArticleProps) {
   return (
-    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex-col items-center">
+    <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg gap-2 flex-col items-center">
       <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">{title}</h3>
+
+      <div className="w-full justify-end gap-2 pr-5 hidden sm:flex">
+        {
+          tags.map((tag) => <span key={tag} className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 dark:text-gray-200 text-xs uppercase font-bold">{tag}</span>)
+        }
+      </div>
 
       <div className="grid gap-1">
         {text.map((content) => <p key={content} className="text-alura-200 dark:text-gray-400">{content}</p>)}
       </div>
 
-      {image && <img src={image} className="sm:p-4"/>}
+      {image && <img src={image} className="sm:p-4" />}
       {image && alt && <span className="sr-only">{alt}</span>}
     </div>
   )
